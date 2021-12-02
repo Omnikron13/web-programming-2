e3a61d41ee3e5b5ec5ca48fc9552727fa4af6077
Joey Sabey <joey.sabey@gmx.com>
2021-12-02 23:34:42 +0000

    Add short delays when mousing into/out of the menu to make it feel less jittery

--- public/js/dropdown.js
+++ public/js/dropdown.js
@@ -4,13 +4,29 @@ let dropdowns = document.querySelectorAll("details.dropdown");
 for(n = 0; n < dropdowns.length; n++) {
     // Open menu on mouse over
     dropdowns[n].addEventListener("mouseenter", event => {
-        event.target.setAttribute("open", "");
+        // Track mouse over state
+        event.target.isMouseOver = true;
+
+        // Delay menu closing until the pointer is over the menu for a short time
+        setTimeout(() => {
+            if(event.target.isMouseOver)
+                event.target.setAttribute("open", "");
+        }, 500);
+
         event.stopPropagation();
     });
 
     // Close menu on mouse leave
     dropdowns[n].addEventListener("mouseleave", event => {
-        event.target.removeAttribute("open");
+        // Track mouse over state
+        event.target.isMouseOver = false;
+
+        // Delay menu closing until the pointer is out of the menu for a short time
+        setTimeout(() => {
+            if(!event.target.isMouseOver)
+                event.target.removeAttribute("open");
+        }, 750);
+
         event.stopPropagation();
     });
 }

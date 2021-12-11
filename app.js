3ece24bac9b4c9e119e90562b3bf59ff2c6cac2e
Joey Sabey <joey.sabey@gmx.com>
2021-12-11 16:41:07 +0000

    Add basic static route to server content in public

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ const express = require('express');
 
 const app = express();
 
+// Serve anything found in the public dir statically by default
+app.use(express.static(__dirname + '/public'));
+
 app.listen(PORT, (error) => {
     if(error) {
         console.error("Failed to start listening on port " + PORT);

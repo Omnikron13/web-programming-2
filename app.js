9acef30633d15a5773a79828e9fbee93e9da8653
Joey Sabey <joey.sabey@gmx.com>
2021-12-11 16:19:23 +0000

    Added (very) basic skeleton of app.js

---
+++ app.js
@@ -0,0 +1,13 @@
+const PORT = 8080;
+
+const express = require('express');
+const app = express();
+
+app.listen(PORT, (error) => {
+    if(error) {
+        console.error("Failed to start listening on port " + PORT);
+        console.error(error);
+        return;
+    }
+    console.log("Listening on port " + PORT);
+});

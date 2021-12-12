8b004b55a12bdac72cef9c47a4b3df7848ff1a5c
Joey Sabey <joey.sabey@gmx.com>
2021-12-12 03:11:49 +0000

    comment

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ const signup  = require('./signup');
 // Initialise express
 const app = express();
 
+// Process POST input from form submission and JS fetch
 app.use(express.urlencoded({extended:true}));
 app.use(express.json());
 
